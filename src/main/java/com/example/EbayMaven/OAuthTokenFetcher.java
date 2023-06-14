6cbc5c5e5d837b8d3245e6a1102919c18130bf81
genia <guk.genia@gmail.com>
2023-06-15 01:32:36 +0300

    helloworld работает

--- src/main/java/com/example/EbayMaven/OAuthTokenFetcher.java
+++ src/main/java/com/example/EbayMaven/OAuthTokenFetcher.java
@@ -1,8 +1,6 @@
 package com.example.EbayMaven;
 
-import com.ebay.auth.oauthclient.CredentialUtil;
-import com.ebay.auth.oauthclient.model.Environment;
-import com.ebay.auth.oauthclient.model.OAuthResponse;
+/*
 
 public class OAuthTokenFetcher {
 
@@ -13,17 +11,17 @@ public class OAuthTokenFetcher {
             String ruName = "YOUR_RUNAME";
 
             // Set the environment to Sandbox
-            Environment environment = Environment.SANDBOX;
+            //   Environment environment = Environment.SANDBOX;
 
             // Fetch OAuth token
-            OAuthResponse response = CredentialUtil.fetchToken(clientId, clientSecret, ruName, environment);
-            String oAuthToken = response.getAccessToken();
+            //OAuthResponse response = CredentialUtil.fetchToken(clientId, clientSecret, ruName, environment);
+           // String oAuthToken = response.getAccessToken();
 
             // Print the OAuth token
-            System.out.println("OAuth Token: " + oAuthToken);
+            //System.out.println("OAuth Token: " + oAuthToken);
         } catch (Exception e) {
             System.out.println("Failed to fetch OAuth token.");
             e.printStackTrace();
         }
     }
-}
+}*/
